87b4857b4b400972294f79ad3681b227d16d456d
JennaLH <jenna.howe@live.co.uk>
2015-04-14 16:45:08 -0400

    Download all constit geojson files from mapit

--- get_constit_polys.py
+++ get_constit_polys.py
@@ -1,16 +1,27 @@
 import json
+from urllib import urlretrieve
 
 with open('/home/jenna/Documents/twfy/data/WMC.json') as f:
     raw_constits = json.load(f)
     
     constits = {}
     for code in raw_constits:
-        name = raw_constits[constit]['name']
+        name = raw_constits[code]['name']
         constits[name] = code
-                
-print constits
 
+base_url = 'http://mapit.mysociety.org/area/'
 
+def prettify(name):
+    return name.lower().replace(' ', '_').replace(',', '')
+
+for name in constits:
+    code = constits[name]
+    name = prettify(name)
+
+    url = base_url + code + '.geojson'
+    dest = '/home/jenna/Documents/twfy/data/' + name + '.geojson'
+    print "downloading geometry for: " + name
+    urlretrieve(url, dest)
 
 
 
