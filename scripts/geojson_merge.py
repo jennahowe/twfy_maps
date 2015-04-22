b290c0868394e14aaea27b0f41600a34b6a4090d
JennaLH <jenna.howe@live.co.uk>
2015-04-22 12:54:44 -0400

    Change single quotes for double quotes

--- scripts/geojson_merge.py
+++ scripts/geojson_merge.py
@@ -1,29 +1,41 @@
 import json
 import os
+import requests
 
-path = '/home/jenna/Documents/twfy/data/constits/'
+path = "/home/jenna/Documents/twfy/data/constits/"
 filenames = os.listdir(path)
 
 def fix_format(data, constit):
-    return "{type: Feature, properties: {name: %s}, geometry: %s}" % (constit, data)
+    dict_ = {"type":"Feature",
+             "properties":{"name":constit}, 
+             "geometry":data,
+            }
+    return dict_
 
 data = {}
 for filename in filenames:
-    with open (path + filename, 'r') as f:
+    with open (path + filename, "r") as f: 
         print "reading: " + filename
         file_ = f.read()
+        file_ = json.loads(file_)
     file_ = fix_format(file_, filename[:-8])
     data[filename[:-8]] = file_
 
-constits_geo = ''
+i = 0
+constits_geo = ""
 for constit in data:
-    geo = data[constit]
+    geo = json.dumps(data[constit])
     constits_geo += geo
 
-all_geo = '{type: FeatureCollection, features: [%s]}' % constits_geo
+    if i < len(filenames) - 1:
+        constits_geo += ", "
+    i += 1
 
-with open (path + 'combined.geojson', 'a') as f:
+all_geo = '{"type":"FeatureCollection","features":['
+all_geo += constits_geo + ']}'
+#all_geo = json.loads(all_geo.replace(' ', ''))
+all_geo = json.loads(all_geo)
+
+with open (path + "combined2.geojson", "a") as f:
     json.dump(all_geo, f)
-    
-
 
