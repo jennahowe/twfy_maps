6063b6c46a46e7e42db5cf11a91bb91491693c6f
Jenna Howe <jenna.howe@live.co.uk>
2016-02-23 17:00:56 +0000

    Get constituency data from twfy api

--- index.py
+++ index.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
+from twfy import TWFY
+import json
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    twfy = TWFY('DDuejwBGcGSrDBEfmVGZo9Yz')
+    mp_list = json.loads(twfy.api.getMPs(output='js'), 'iso-8859-1')
+    constituency_data = {}
+
+    for mp in mp_list:
+        constituency_data[mp["constituency"]] = {'name': mp["name"],
+                                              'party': mp["party"], 
+                                              'member_id': mp["member_id"],
+                                              }
+    return render_template('index.html', constituency_data=constituency_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
