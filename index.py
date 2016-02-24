ebce4b2d28846e098f0963e820039e0ac0166268
Jenna Howe <jenna.howe@live.co.uk>
2016-02-24 16:54:01 +0000

    Show MP and party in box for selected constit

--- index.py
+++ index.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 def index():
     twfy = TWFY('DDuejwBGcGSrDBEfmVGZo9Yz')
     mp_list = json.loads(twfy.api.getMPs(output='js'), 'iso-8859-1')
-    constituency_data = {}
+    constit_data = {}
 
     for mp in mp_list:
-        constituency_data[mp["constituency"]] = {'name': mp["name"],
-                                              'party': mp["party"], 
-                                              'member_id': mp["member_id"],
-                                              }
-    return render_template('index.html', constituency_data=constituency_data)
+        constit_data[mp["constituency"]] = {'mp_name': mp["name"],
+                                       'party': mp["party"], 
+                                       'member_id': mp["member_id"],
+                                       }
+    return render_template('index.html', constit_data=constit_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
